e230e393211199e08e66e286a00ccca7c1e368ad
Nico Beierle <nico.beierle@gmail.com>
2024-07-16 10:56:40 +0200

    feat: add styles and header to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,34 @@
   import { base } from '$app/paths';
 </script>
 
-<a href={`${base}/doc`}>Doc</a>
-<a href={`${base}/slides`}>Slides</a>
+<div class="cover">
+  <h1>MDocSlides</h1>
+  <nav>
+    <a href={`${base}/doc`} class="button button-primary">Pages</a>
+    <a href={`${base}/slides`} class="button button-primary">Slides</a>
+  </nav>
+</div>
+
+<style>
+  .cover {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    gap: 2rem;
+  }
+
+  h1 {
+    margin: 0;
+  }
+
+  nav {
+    display: flex;
+    gap: 2rem;
+  }
+
+  a {
+    font-size: 1.2rem;
+  }
+</style>
