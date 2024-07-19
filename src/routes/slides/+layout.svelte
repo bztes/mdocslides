a0e6c2632751c67e0157f9636097b59422138482
Nico Beierle <nico.beierle@gmail.com>
2024-07-19 13:17:14 +0200

    feat: add search dialog to slides

--- src/routes/slides/+layout.svelte
+++ src/routes/slides/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+  import Search from '$lib/ui/Search.svelte';
   import { setContext } from 'svelte';
 
   let { children } = $props();
 
   setContext('view', 'slides');
 </script>
+
+<Search></Search>
 
 <main>
   {@render children()}
